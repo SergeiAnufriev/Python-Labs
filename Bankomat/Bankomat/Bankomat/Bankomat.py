d1c421d2e64c831f661da94f31bbf393ef24654b
Sergei Anufriev <seregioas@gmail.com>
2015-09-30 20:30:45 +0300

    убрал вывод банкнот, которые банкомат не выдаёт

--- Bankomat/Bankomat/Bankomat/Bankomat.py
+++ Bankomat/Bankomat/Bankomat/Bankomat.py
@@ -1,37 +1,69 @@
-summ = input ('Vvedite summu: ')
+summ = int(input ('Vvedite summu: '))
 
 if summ < 0:
     print ('Game over')
 else:             
-    a = ( summ / 500)
+    a = ( summ // 500)
     b = summ - (500 * a)
-    print (a, ' x 500$')
-
-    c = ( b / 200)
+    
+    c = ( b // 200)
     d = b - (200 * c)
-    print (c, ' x 200$')
-
-    e = ( d / 100)
+ 
+    e = ( d // 100)
     f = d - (100 * e)
-    print (e, ' x 100$')
+   
+    g = ( f // 50)
+    k = f - (50 * g)  
 
-    g = ( f / 50)
-    k = f - (50 * g)
-    print (g, ' x 50$')
-
-    w = ( k / 20)
+    w = ( k // 20)
     q = k - (20 * w)
-    print (w, ' x 20$')
-
-    r = ( q / 10)
+   
+    r = ( q // 10)
     t = q - (10 * r)
-    print (r, ' x 10$')
-
-    y = ( t / 5)
-    u = t - (5 * y)
-    print (y, ' x 5$')
   
-    i = ( u / 1)
+    y = ( t // 5)
+    u = t - (5 * y)
+   
+    i = ( u // 1)
     o = u - (1 * i)
-    print (i, ' x 1$')
+
+if a > 0:
+    print (' ', a, 'x 500$')
+else:
+    z = 6
+    
+if c > 0:
+   print (' ', c, 'x 200$')
+else:
+     z = 6
+
+if e > 0:
+   print (' ', e, 'x 100$')
+else:
+     z = 6
+
+if g > 0:
+   print (' ', g, 'x 50$')
+else:
+     z = 6
+
+if w > 0:
+    print (' ', w, 'x 20$')
+else:
+     z = 6
+
+if r > 0:
+   print (' ', r, 'x 10$')
+else:
+     z = 6
+
+if y > 0:
+   print (' ', y, 'x 5$')
+else:
+     z = 6
+
+if i > 0:
+   print (' ', i, 'x 1$')
+else:
+     z = 6
 
