8699d10f4364837cb03ab83e00cbdbe1bed014fc
Sergei Anufriev <seregioas@gmail.com>
2015-10-14 22:13:15 +0300

    Сделал возможным повторно выполнять программу без перезапуска

--- Bankomat While/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ Bankomat While/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,18 +1,29 @@
-summ = str(input ('Vvedite summu: '))
+﻿   
+exit = 1
+while exit == 1:  
+  
+    summ = float(input ('Vvedite summu: '))
 
-ostatok = int(summ)
+    ostatok = summ
 
-nominals = [500, 200, 100, 50, 20, 10, 5, 1]
-nominalIndex = 0
+    nominals = [500, 200, 100, 50, 20, 10, 5, 1]
+    nominalIndex = 0
 
-while ostatok > 0 and nominalIndex < len (nominals):
-    x = ostatok // nominals[nominalIndex]
-    ostatok = ostatok - (x * nominals[nominalIndex])
+    while ostatok > 0 and nominalIndex < len (nominals):
+        x = ostatok // nominals[nominalIndex]
+        ostatok = ostatok - (x * nominals[nominalIndex])
 
-    if x > 0:
-       print (x, ' x ', nominals[nominalIndex], '$')
+        if x > 0:
+           print (x, ' x ', nominals[nominalIndex], '$')
        
-    nominalIndex = nominalIndex + 1
+        nominalIndex = nominalIndex + 1
+
+    print ('Hotite povtorit? vvedite 1')
+    print ('Hotite zaverchit rabatu? vvedite 2')
+
+    exit = int (input( 'Vvedite 1 and 2: '))
+
+
     
 
 
