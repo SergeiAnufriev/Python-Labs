4ddbf330d5ea32a04288f751ead830c5973b68d7
Sergei Anufriev <seregioas@gmail.com>
2015-10-14 22:14:08 +0300

    Сделал возможным повторно выполнять программу без перезапуска

--- Laba5/Comp ugadai/Ugadai chislo comp/Ugadai chislo comp/Ugadai_chislo_comp.py
+++ Laba5/Comp ugadai/Ugadai chislo comp/Ugadai chislo comp/Ugadai_chislo_comp.py
@@ -1,29 +1,36 @@
 ﻿import random
 
-print ('Ugadai chislo')
-print ('''Vam nugno vvesti 2 chisla, odno bolche drugogo,
-zatem computer ugadaet chislo iz zadannogo diapazona,
-s vachimi podskazkami''')
-
-nachalnoeChislo = int (input('Vvedite nachalnoe chislo: '))
-konechnoeChislo = int (input('Vvedite konechnoe chislo: '))
-otvet = int (input ('Zagadaite chuslo iz zadannogo diapazona: '))
-computerRandom = random.randint(nachalnoeChislo, konechnoeChislo)
-
-
-while computerRandom != otvet:
-    print ('otvet', computerRandom, '?')
-    print ('Esli danoe chislo < zagadannogo, nagmite 1')
-    print ('Esli danoe chislo > zagadannogo, nagmite 2')
-    podskazka = int (input('Vvedite 1 ili 2: '))
-
-    if podskazka == 1:
-        computerRandom = random.randint(computerRandom + 1, konechnoeChislo)
+exit = 1
+while exit == 1:
     
-    if podskazka == 2:
-        computerRandom = random.randint(nachalnoeChislo, computerRandom - 1)
+    print ('Ugadai chislo')
+    print ('''Vam nugno vvesti 2 chisla, odno bolche drugogo,
+    zatem computer ugadaet chislo iz zadannogo diapazona,
+    s vachimi podskazkami''')
 
-print ('Vache chislo ugadali, otvet = ', computerRandom)
+    nachalnoeChislo = int (input('Vvedite nachalnoe chislo: '))
+    konechnoeChislo = int (input('Vvedite konechnoe chislo: '))
+    otvet = int (input ('Zagadaite chuslo iz zadannogo diapazona: '))
+    computerRandom = random.randint(nachalnoeChislo, konechnoeChislo)
+
+
+    while computerRandom != otvet:
+        print ('otvet', computerRandom, '?')
+        print ('Esli danoe chislo < zagadannogo, nagmite 1')
+        print ('Esli danoe chislo > zagadannogo, nagmite 2')
+        podskazka = int (input('Vvedite 1 ili 2: '))
+
+        if podskazka == 1:
+            computerRandom = random.randint(computerRandom + 1, konechnoeChislo)
+    
+        if podskazka == 2:
+            computerRandom = random.randint(nachalnoeChislo, computerRandom - 1)
+
+    print ('Vache chislo ugadali, otvet = ', computerRandom)
+    print ('Hotite povtorit? vvedite 1')
+    print ('Hotite zaverchit rabatu? vvedite 2')
+
+    exit = int (input( 'Vvedite 1 and 2: '))
      
         
          
