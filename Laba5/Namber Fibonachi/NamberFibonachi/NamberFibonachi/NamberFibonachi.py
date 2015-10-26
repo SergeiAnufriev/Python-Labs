59d14b4839929004ea2d1e2a7b49e8b81d90df1e
Sergei Anufriev <seregioas@gmail.com>
2015-10-26 18:13:44 +0200

    Создал программу для чисел Фибоначчи

---
+++ Laba5/Namber Fibonachi/NamberFibonachi/NamberFibonachi/NamberFibonachi.py
@@ -0,0 +1,22 @@
+﻿
+print('''Polzovatel vvodit poryadkovi nomer elementa posledovatelnosti Fibonachi,
+ a programma vuvodit znachenie dannogo elementa''')
+
+namber = int(input("Vvedite poryadkovi nomer elementa posledovatelnosti Fibonachi: "))
+
+fib1 = 0
+fib2 = 1
+schetchik = 0
+i = 0
+
+while i < namber - 1 :
+	sumFib = fib1 + fib2
+	fib1 = fib2
+	fib2 = sumFib
+	i += 1
+
+if i == namber - 1:
+	print(fib1)
+
+else:
+	print(fib1)
