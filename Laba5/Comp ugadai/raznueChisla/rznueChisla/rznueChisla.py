d59d5748043d2a467890fc35d80a434fd323689d
Sergei Anufriev <seregioas@gmail.com>
2015-10-26 18:14:20 +0200

    Программа для подсчёта различных чисел в заданном числе

---
+++ Laba5/Comp ugadai/raznueChisla/rznueChisla/rznueChisla.py
@@ -0,0 +1,33 @@
+﻿print("Programma nachodit colichestvo razlichnuch namder v zadannom chisle")
+
+chislo = input('Input namber: ')
+count = len(chislo)
+print ("In your namber sostoit from: " + str(count))
+
+chislo = int(chislo)
+
+strb = str(chislo % 10)
+
+#for _ in range(count-1):
+	
+#	chislo //= 10
+#	s = str(chislo % 10)
+
+#	if s not in strb:
+#		strb += s;
+
+#print("Resalt: " + str(len(strb)))
+
+for _ in range(count-1):
+	
+	chislo //= 10
+	s = str(chislo % 10)
+	
+	if s not in strb:
+		s = int(s)
+		strb = int(srtb)
+
+		if s > strb:
+			strb = s;
+
+print("Resalt: " + str(strb))
