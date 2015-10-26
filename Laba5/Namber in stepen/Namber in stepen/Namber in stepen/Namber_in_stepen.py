a68072d1a312dadcf0def96d6a0da5d89833f651
Sergei Anufriev <seregioas@gmail.com>
2015-10-26 18:13:10 +0200

    Создал программу для возведения числа в степень

---
+++ Laba5/Namber in stepen/Namber in stepen/Namber in stepen/Namber_in_stepen.py
@@ -0,0 +1,5 @@
+print("Programma pozvolyet vozvesti cislo v naturalnuy stepen")
+namber = int(input("Input namber: "))
+stepen = int(input("Input stepen"))
+rezult = namber ** stepen
+print(namber, " in stepen ", stepen, " = ", rezult)
