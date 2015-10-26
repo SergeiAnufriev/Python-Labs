c3761418c00e90edaf6fe35a66719629fde199c0
Sergei Anufriev <seregioas@gmail.com>
2015-10-26 18:15:39 +0200

    Программа для определения знаков и суммы чисел в заданном числе

---
+++ Laba5/Comp ugadai/chisla/chisla/chisla.py
@@ -0,0 +1,12 @@
+﻿chislo = input('Input namber: ')
+count = len(chislo)
+print ("In your namber sostoit from: " + str(count))
+sum = 0
+chislo = int(chislo)
+
+for _ in range(count):
+	#sum1 = chislo - ((chislo // 10)*10)
+	sum += chislo % 10
+	chislo //= 10
+
+print ('Summa cifr zadannogochisla = ', sum)
