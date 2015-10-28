aa0db7bd346481ef0d3a1071507581665d4aaee4
Sergei Anufriev <seregioas@gmail.com>
2015-10-28 11:08:05 +0200

    Программа для нахождения НОД и НОК чисел

---
+++ Laba5/NOD_NOK/NOD_NOK/NOD_NOK/NOD_NOK.py
@@ -0,0 +1,27 @@
+﻿print("Program nachodit NOD and NOK")
+
+a = int(input("Input namber a: "))
+b = int(input("Input namber b: "))
+
+def lcm(a, b):
+	m = a * b
+	while a != 0 and b != 0:
+		if a > b:
+			a %= b
+		else:
+			b %= a
+	return m // (a + b)
+
+
+def nod(a, b):
+	while a!=0 and b!=0:
+		if a > b:
+			a = a % b
+		else:
+			b = b % a
+	return a + b
+
+print("NOD = ", nod(a, b))
+print('НОК = ', lcm(a, b))
+
+
