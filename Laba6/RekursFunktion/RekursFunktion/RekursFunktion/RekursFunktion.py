976254260e73833ea1d4f1b654d3f78c82f75978
Sergei Anufriev <seregioas@gmail.com>
2015-11-09 23:33:33 +0300

    Рекурсивная функция для x^n/n!

---
+++ Laba6/RekursFunktion/RekursFunktion/RekursFunktion/RekursFunktion.py
@@ -0,0 +1,13 @@
+﻿import mymath
+
+def f(x, n):
+	
+	return mymath.pow(x, n) / mymath.faktorial(n)
+
+print("Program rekursivnoi funttion, vuchislyoychay velichinu x^n/n")
+
+x = int(input("Input x: "))
+n = int(input("Input n: "))
+
+print(f(x, n))
+
