ee870cf966061e82d7549b4c0e9a1253f2826eee
Sergei Anufriev <seregioas@gmail.com>
2015-11-10 22:34:34 +0300

    Числа Фибоначчи рекурсией

---
+++ Laba6/Namber Fibonachi Rekursiv/NamberFibonachi/NamberFibonachi/NamberFibonachiRekursiv.py
@@ -0,0 +1,13 @@
+﻿
+print('''Polzovatel vvodit poryadkovi nomer elementa posledovatelnosti Fibonachi,
+ a programma vuvodit znachenie dannogo elementa''')
+
+n = int(input("Vvedite poryadkovi nomer elementa posledovatelnosti Fibonachi: "))
+
+def fib(n, a = 0, b = 1):
+	if n == 0:
+		return a
+	else:
+		return fib(n - 1, b, a + b)
+
+print (fib(n))
